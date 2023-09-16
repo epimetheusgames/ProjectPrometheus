7da961efc6bd21c612715703332bf3eb22be3447
Gurfy Person <cbates8923@student.pps.net>
2023-09-16 16:05:21 -0700

    Camera zoom is now smooth when stopping

--- Objects/PlayerManager.gd
+++ Objects/PlayerManager.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 func _process(delta):
+	# Camera follows player.
 	$Camera.position += ($Player.position - $Camera.position) * 0.1
 	
 	var player_vel = 0
 	
+	# Camera zooms out as player moves faster.
 	if $Camera.zoom.x > 0.75:
-		player_vel = 1 - abs($Player.velocity.x) / 50
+		player_vel = 1 - abs($Player.velocity.x) / 100
 	else:
 		player_vel = 0.75
-	$Camera.zoom = Vector2(player_vel, player_vel)
+		
+	$Camera.zoom += (Vector2(player_vel, player_vel) - $Camera.zoom) * 0.05
 	print($Camera.zoom)
