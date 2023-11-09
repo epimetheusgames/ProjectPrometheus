45f266ecd96545f00fc70ac6c70c89047004dd49
Carson Bates <blackhole243243@gmail.com>
2023-11-09 12:30:17 -0800

    Added a new 3dish wall

--- Scripts/ObjectScripts/Player.gd
+++ Scripts/ObjectScripts/Player.gd
@@ -12,6 +12,8 @@ extends CharacterBody2D
 @export var speed_hard_cap = 3.5
 @export var jump_speed_boost = 1.1
 var dont_apply_friction = false
+var could_jump = false
+var just_jumped = false
 
 # Figure out the velocity based on the inputs.
 func getInputVelocity(can_jump):
@@ -30,29 +32,41 @@ func checkJump():
 	return Input.is_action_just_pressed("jump")
 	
 func canJump():
-	return is_on_floor()
+	return is_on_floor() || $CoyoteJumpTimer.time_left > 0
 
 func _physics_process(_delta):
 	# Apply gravity
 	velocity.y += gravity
+	
 	dont_apply_friction = false
+	just_jumped = false
 	
 	# Apply keyboard inputs.
 	var can_jump = canJump()
 	var input_velocity = getInputVelocity(can_jump)
 	velocity.x += input_velocity
-	if checkJump() and canJump():
+	
+	# Check if we can jump
+	if checkJump() and can_jump:
+		just_jumped = true
 		velocity.x *= jump_speed_boost
 		velocity.y = -jump_vel
+		
+	# Implement coyote jumping system.
+	if could_jump && !can_jump:
+		$CoyoteJumpTimer.start()
 		
 	# If the player is holding the jump button, apply a slight upwards push.
 	if Input.is_action_pressed("jump"):
 		velocity.y -= jump_push_force
 		
+	# Hard cap the speed to supress speed glitches.
 	if abs(velocity.x) > speed_hard_cap:
 		velocity.x = max_speed if velocity.x > 1 else -max_speed
 		
+	# Apply friction.
 	if input_velocity == 0:
+		# Don't apply friction if the player is moving.
 		if can_jump && (abs(velocity.x) != max_speed && Input.get_axis("left", "right") == 0):
 			velocity.x /= friction_force
 		else:
@@ -63,6 +77,12 @@ func _physics_process(_delta):
 	
 	# Collisions.
 	move_and_slide()
+	
+	# For coyote jumping, check if we can jump.
+	if can_jump && !just_jumped:
+		could_jump = true
+	else:
+		could_jump = false
 
 func _on_area_2d_area_entered(area):
 	if area.name == "DeathZone":
