60bf6bdddf186cff3879ed5fa82acc4bdbe7131c
Carson Bates <blackhole243243@gmail.com>
2023-11-08 22:28:28 -0800

    New non-transparent metal bar tilemap which isn't very different.

--- Scripts/ObjectScripts/Player.gd
+++ Scripts/ObjectScripts/Player.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 # Speed multiplier for the player
-@export var speed = 0.2
-@export var jump_vel = 10
+@export var speed = 0.4
+@export var jump_vel = 6
 @export var gravity = 0.5
 @export var friction_force = 1.2
 @export var air_friction_force = 1.01
-@export var max_speed = 5
+@export var max_speed = 3
 @export var max_air_speed = 4.5
+@export var jump_push_force = 0.2
+@export var speed_hard_cap = 3.5
 var dont_apply_friction = false
 
 # Figure out the velocity based on the inputs.
@@ -41,10 +43,14 @@ func _physics_process(_delta):
 	if checkJump() and canJump():
 		velocity.y = -jump_vel
 		
-	if abs(velocity.x) >= max_speed:
-		velocity.x -= 0.001
+	# If the player is holding the jump button, apply a slight upwards push.
+	if Input.is_action_pressed("jump"):
+		velocity.y -= jump_push_force
 		
-	if input_velocity == 0 || (can_jump && ((input_velocity > 0 && velocity.x < 0) || (input_velocity < 0 && velocity.x > 0))):
+	if abs(velocity.x) > speed_hard_cap:
+		velocity.x = max_speed if velocity.x > 1 else -max_speed
+		
+	if input_velocity == 0:
 		if can_jump && (abs(velocity.x) != max_speed && Input.get_axis("left", "right") == 0):
 			velocity.x /= friction_force
 		else:
@@ -57,4 +63,5 @@ func _physics_process(_delta):
 	move_and_slide()
 
 func _on_area_2d_area_entered(area):
-	position = get_parent().get_parent().get_node("RespawnPos").position
+	if area.name == "DeathZone":
+		position = get_parent().get_parent().get_node("RespawnPos").position
