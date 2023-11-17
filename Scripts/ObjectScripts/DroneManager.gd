2c01003b763cdc225ded663cb38a2a30ebf13785
Carson Bates <blackhole243243@gmail.com>
2023-11-16 17:35:42 -0800

    Added drone line stuff

--- Scripts/ObjectScripts/DroneManager.gd
+++ Scripts/ObjectScripts/DroneManager.gd
@@ -1,14 +1,35 @@
 extends Node2D
 
 var current_line_point = 0
+var movement_velocity = Vector2.ZERO
 
-func between(a, b):
-	return b - ((b - a) / 2)
+@export var velocity_smoothing = 0.01
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func smooth(a, b, smoothing):
+	return (a + ((b - a) * smoothing))
+
+func _process(delta):
 	var player = get_parent().get_node("Player").get_node("Player")
 	
-	var direction_to_player_reversed = (player.position - (position + $DroneTurret1.position)).normalized()
-	var direction_to_player_radians = -atan2(direction_to_player_reversed.x, direction_to_player_reversed.y)# - (1.0/2.0 * PI)
-	$DroneTurret1.rotation = direction_to_player_radians
+	var direction_to_player = (player.position - (position + $Drone.position)).normalized()
+	var direction_to_player_radians = -atan2(direction_to_player.x, direction_to_player.y)
+	$Drone/DroneTurret1.rotation = direction_to_player_radians - $Drone.rotation
+	
+	var direction_to_next_point = ($DronePatrolPoints.points[current_line_point] - $Drone.position).normalized() * delta * 60
+
+	movement_velocity = Vector2(smooth(movement_velocity.x, direction_to_next_point.x, velocity_smoothing * 1.5),
+								smooth(movement_velocity.y, direction_to_next_point.y / 1.5, velocity_smoothing))
+	
+	$Drone.position += movement_velocity
+	$Drone.rotation = movement_velocity.x / 3
+	
+	if $Drone.position.distance_to($DronePatrolPoints.points[current_line_point]) < 30:
+		if current_line_point < $DronePatrolPoints.points.size() - 1:
+			current_line_point += 1
+			
+			if current_line_point == 1000:
+				var new_drone = duplicate()
+				new_drone.get_node("Drone").position = Vector2.ZERO
+				get_parent().add_child(new_drone)
+		else:
+			current_line_point = 0
