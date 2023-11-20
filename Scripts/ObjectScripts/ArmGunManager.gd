70bc19c3901d10d9b2fa90bc8342cc92c60be755
Carson Bates <blackhole243243@gmail.com>
2023-11-20 08:29:36 -0800

    Arm movements completed

--- Scripts/ObjectScripts/ArmGunManager.gd
+++ Scripts/ObjectScripts/ArmGunManager.gd
@@ -10,6 +10,8 @@ func _process(delta):
 		$Line2D.points[2].x = mouse_pos.x / 100 + (mouse_pos.y / 80) - 20
 		$Line2D.points[2].y = -25 - (mouse_pos.x / 150) + (mouse_pos.y / 200)
 		
-		var mouse_direction = (mouse_pos - $Line2D.points[2]).normalized()
+		var mouse_direction = (mouse_pos + $Line2D.points[2]).normalized()
 		
 		$Line2D.points[3] = $Line2D.points[2] + (mouse_direction * 20)
+	else:
+		visible = false
