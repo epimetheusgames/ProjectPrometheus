6315bed386d6318567fe0cef073b7e2c0a3e0a41
Carson Bates <blackhole243243@gmail.com>
2023-12-05 19:24:41 -0800

    Fixed direction of arm gun

--- Scripts/ObjectScripts/ArmGunManager.gd
+++ Scripts/ObjectScripts/ArmGunManager.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var loaded_bullet = preload("res://Objects/StaticObjects/PlayerBullet.tscn")
+@onready var point_1_start_x = $Line2D.points[1].x
+
 var active = false
 
 func _process(delta):
@@ -9,10 +11,15 @@ func _process(delta):
 		
 		var mouse_pos = get_viewport().get_mouse_position() - Vector2(get_viewport_rect().size.x / 2, get_viewport_rect().size.y / 2)
 		
+		if get_parent().previous_direction == -1:
+			mouse_pos.x = -mouse_pos.x
+		
 		$Line2D.points[2].x = mouse_pos.x / 100 + (mouse_pos.y / 80) - 20
 		$Line2D.points[2].y = -25 - (mouse_pos.x / 150) + (mouse_pos.y / 200)
 		
-		var mouse_direction = (get_global_mouse_position() - (get_parent().position + $Line2D.points[2])).normalized()
+		var reversed_points_2 = Vector2(-$Line2D.points[2].x, $Line2D.points[2].y)
+		var mouse_direction = (get_global_mouse_position() - (get_parent().position + ($Line2D.points[2] if get_parent().previous_direction == 1 else reversed_points_2))).normalized()
+		var reversed_mouse_dir = Vector2(-mouse_direction.x, mouse_direction.y)
 		
 		# For controller
 		if len(Input.get_connected_joypads()) > 0:
@@ -21,7 +28,14 @@ func _process(delta):
 			mouse_direction = Vector2(controller_joy_dir_x, controller_joy_dir_y)
 			mouse_pos = mouse_direction * 50
 		
-		$Line2D.points[3] = $Line2D.points[2] + (mouse_direction * 20)
+		$Line2D.points[3] = $Line2D.points[2] + ((mouse_direction if get_parent().previous_direction == 1 else reversed_mouse_dir) * 20)
+		
+		if get_parent().previous_direction == -1:
+			$Line2D.points[3].x = -$Line2D.points[3].x
+			$Line2D.points[2].x = -$Line2D.points[2].x
+			$Line2D.points[1].x = -point_1_start_x
+		else:
+			$Line2D.points[1].x = point_1_start_x
 		
 		if Input.is_action_just_pressed("mouse_click") || Input.is_action_just_pressed("attack"):
 			var bullet = loaded_bullet.instantiate()
--- Scripts/ObjectScripts/WallTurret.gd
+++ Scripts/ObjectScripts/WallTurret.gd
@@ -5,11 +5,14 @@ extends Node2D
 @onready var graphics_efficiency = get_parent().graphics_efficiency
 
 func _process(delta):
+	if $Sprite2D.scale.x == -1:
+		$Turret.scale.y = -1
+	
 	var player = get_parent().get_node("Player").get_node("Player")
 	
 	var direction_to_player = (player.position - position).normalized()
 	var direction_to_player_radians = -atan2(direction_to_player.x, direction_to_player.y) 
-	$Turret.rotation = direction_to_player_radians
+	$Turret.rotation = direction_to_player_radians - (1.0/2.0 * PI)
 	
 	var distance_to_player = player.position.distance_to(position)
 	
