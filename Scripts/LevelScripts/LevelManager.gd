f9620abfef97249fda2ee6ccf174c8668ba8a73f
Carson Bates <blackhole243243@gmail.com>
2024-01-15 17:19:13 -0800

    Added light occluder to drills

--- Scripts/LevelScripts/LevelManager.gd
+++ Scripts/LevelScripts/LevelManager.gd
@@ -7,12 +7,16 @@ var graphics_efficiency = false
 var is_max_level = true
 var easy_mode = false
 var deaths_this_level = 0
+@export var lights_off = false
 
 func _ready():
 	if graphics_efficiency:
 		$CanvasModulate.color = Color(0.8, 0.8, 0.8, 1)
 	else:
-		$CanvasModulate.color = Color(0.5, 0.5, 0.5, 1)
+		if lights_off:
+			$CanvasModulate.color = Color(0.3, 0.3, 0.3, 1)
+		else:
+			$CanvasModulate.color = Color(0.5, 0.5, 0.5, 1)
 
 func _on_ambiant_background_finished():
 	$AmbiantBackground.play()
