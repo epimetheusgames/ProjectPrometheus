8aaee7439652ae34780cbfbced3aa1b25a569b29
Elouan Grimm <elouan.grimm@gmail.com>
2024-01-30 13:25:08 -0800

    level

--- Scripts/ObjectScripts/ArtifactManager.gd
+++ Scripts/ObjectScripts/ArtifactManager.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
+func _ready():
+	$AnimatedSprite2D.play("Animation")
 
 func _on_area_entered(area):
 	if area.name == "PlayerHurtbox":
 		get_parent().points += 10
 		$GPUParticles2D.emitting = true
-		$ArtifactSprite.visible = false
+		$AnimatedSprite2D.visible = false
 		$DestroyTimer.start()
 
 func _on_destroy_timer_timeout():
