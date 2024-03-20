f6c76d76808f6e467343adb9db70f03b08ec54ef
Carson Bates <blackhole243243@gmail.com>
2024-03-20 16:36:35 -0700

    Added new bullet sprite (also bullet particles don't disappear when the bullet hits a wall)

--- Scripts/ObjectScripts/PlayerBulletManager.gd
+++ Scripts/ObjectScripts/PlayerBulletManager.gd
@@ -12,18 +12,26 @@ var velocity = Vector2.ZERO
 var direction = Vector2.ZERO
 
 var graphics_efficiency = false
+var stopped_moving = false
 		
 func _process(delta):
-	position += direction * velocity * (delta * 60)
-	rotation = atan2(direction.y, direction.x) + (1.0/2.0 * PI)
+	if !stopped_moving:
+		position += direction * velocity * (delta * 60)
+		rotation = atan2(direction.y, direction.x) + (1.0/2.0 * PI)
 
 func _on_despawn_timer_timeout():
 	queue_free()
 
 func _on_player_bullet_hurter_body_entered(body):
-	if body.name != "Player":
-		queue_free()
+	if body.name != "Player" && !stopped_moving:
+		$BulletSprite.queue_free()
+		$PlayerBulletHurter.queue_free()
+		$GPUParticles2D.emitting = false
+		stopped_moving = true
 
 func _on_player_bullet_hurter_area_entered(area):
-	if area.name == "DroneHurtbox":
-		queue_free()
+	if area.name == "DroneHurtbox" && !stopped_moving:
+		$BulletSprite.queue_free()
+		$PlayerBulletHurter.queue_free()
+		$GPUParticles2D.emitting = false
+		stopped_moving = true
