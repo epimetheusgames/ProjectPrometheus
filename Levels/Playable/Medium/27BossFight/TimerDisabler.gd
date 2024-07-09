1c862b8372e85940c7acbfc2725de0ccfd96a344
Carson Bates <blackhole243243@gmail.com>
2024-07-08 18:25:04 -0700

    Fixed voicelines players disabling your timer.

--- Levels/Playable/Medium/27BossFight/TimerDisabler.gd
+++ Levels/Playable/Medium/27BossFight/TimerDisabler.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 
 @export var double_get_parent = false
+@export var temporary = false
 
 func _on_area_entered(area):
 	if area.name == "PlayerHurtbox":
@@ -11,3 +12,10 @@ func _on_area_entered(area):
 		else:
 			get_parent().get_parent().no_timer = true
 			get_parent().get_parent().boss = false
+
+func _on_area_exited(area):
+	if area.name == "PlayerHurtbox":
+		if !double_get_parent:
+			get_parent().no_timer = false
+		else:
+			get_parent().get_parent().no_timer = false
