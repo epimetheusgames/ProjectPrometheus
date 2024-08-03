46beaf2a04fa8c82bf6283a8dbac210aa601f259
Carson Bates <blackhole243243@gmail.com>
2024-08-03 12:54:33 -0700

    Fixed more of alejandro's bugs

--- Scripts/ObjectScripts/PressEAreaPopup.gd
+++ Scripts/ObjectScripts/PressEAreaPopup.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
 
+@export var requires_key = false
+
 func _ready():
 	$ControllerSprite2D.visible = false
 
 func _on_area_entered(area):
-	if area.name == "PlayerHurtbox":
+	if area.name == "PlayerHurtbox" && (!requires_key || area.get_parent().has_key):
 		$ControllerSprite2D.visible = true
 
 func _on_area_exited(area):
